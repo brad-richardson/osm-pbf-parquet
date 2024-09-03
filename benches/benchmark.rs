045aab51c5dade60e5cbc6332fc1d7df543c9a9a
Brad Richardson <bradrichardson@meta.com>
2024-09-02 23:23:43 -0400

    Lint

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -18,7 +18,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             .enable_all()
             .build()
             .unwrap();
-        b.to_async(rt).iter(|| bench())
+        b.to_async(rt).iter(bench)
     });
     let _ = fs::remove_dir_all("./test/bench-out/");
 }
